afea4bf5ad77fda531780171b213f00a8331a7c4
Kryštof Řezáč <krystofrezac@gmail.com>
2024-04-01 12:51:01 +0200

    WIP

--- src/content.js
+++ src/content.js
@@ -1,76 +1,97 @@
+const getMaybeLocalizedValue = (maybeLocalized) => {
+  if (typeof maybeLocalized === "string") return maybeLocalized;
+
+  return maybeLocalized.cs;
+};
+
 const injectScript = () => {
   const script = document.createElement("script");
   script.src = chrome.runtime.getURL("src/injected.js");
   (document.head || document.documentElement).appendChild(script);
 };
 
-injectScript();
-
-// TODO: make it beatifull
-const handleLoadLessonForStudent = async (data) => {
-  const {
-    courseId,
-    body: { questionMap },
-  } = data;
-
-  const savedQuestionMap = await chrome.storage.local
-    .get([courseId, "questionMap"])
-    .then((a) => a[courseId]?.questionMap);
-
-  const questionMapWithKnownAnswers = Object.fromEntries(
-    Object.entries(questionMap).filter(([key, value]) => {
-      const {
-        correctAnswerIndex,
-        pairList,
-        correctAnswerIndexList,
-        correctAnswerOrder,
-        tripletList,
-      } = value;
-      if (correctAnswerIndex !== null && correctAnswerIndex !== undefined)
-        return true;
-
-      if (pairList !== null && pairList !== undefined) return true;
-
-      if (
-        correctAnswerIndexList !== null &&
-        correctAnswerIndexList !== undefined
-      )
-        return true;
-
-      if (correctAnswerOrder !== null && correctAnswerOrder !== undefined)
-        return true;
-
-      if (tripletList !== null && tripletList !== undefined) return true;
-
-      return false;
-    }),
-  );
-  console.log("uuHappines: Loaded", questionMapWithKnownAnswers);
-
-  chrome.storage.local.set({
-    [courseId]: {
-      questionMap: { ...savedQuestionMap, ...questionMapWithKnownAnswers },
-    },
-  });
-  alert("uuHappiness: Loaded");
+const sendIconUrl = () => {
+  setInterval(() => {
+    const iconUrl = chrome.runtime.getURL("src/assets/icon128.png");
+    window.postMessage({ type: "iconUrl", iconUrl }, "*");
+  }, 2_000);
 };
 
-window.addEventListener("message", (e) => {
-  if (e.data.type === "loadLessonForStudenResponse")
-    handleLoadLessonForStudent(e.data);
+const listenForMessages = () => {
+  const handleSaveLesson = async (message) => {
+    const { lesson, courseId } = message;
 
-  if (e.data.type === "questionChange") {
-    chrome.runtime.sendMessage(
-      undefined,
-      {
-        type: "questionChange",
-        task: e.data.task,
-        courseId: e.data.courseId,
-      },
-      () => {
-        // Here to silence chrome errors
-        chrome.runtime.lastError;
+    const savedQuestionMap = await chrome.storage.local
+      .get([courseId, "questionMap"])
+      .then((value) => value[courseId]?.questionMap);
+
+    const answerFields = [
+      "correctAnswerIndex",
+      "pairList",
+      "correctAnswerIndexList",
+      "correctAnswerOrder",
+      "tripletList",
+    ];
+
+    const questionMapEntries = Object.entries(lesson.questionMap);
+    const questionMapWithAnswersEntries = questionMapEntries.filter(
+      ([_, question]) => {
+        answerFields.some((answerField) => {
+          const fieldValue = question[answerField];
+          return fieldValue !== undefined && fieldValue !== null;
+        });
       },
     );
-  }
-});
+    const quetsionMapWithAnswers = Object.fromEntries(
+      questionMapWithAnswersEntries,
+    );
+
+    chrome.storage.local.set({
+      [courseId]: {
+        questionMap: { ...savedQuestionMap, ...quetsionMapWithAnswers },
+      },
+    });
+  };
+
+  const handleGetQuestion = async (message) => {
+    const { question: questionToFind, courseId } = message;
+
+    const questionMap = await chrome.storage.local
+      .get([courseId, "questionMap"])
+      .then((value) => value[courseId]?.questionMap);
+
+    if (!questionMap) {
+      window.postMessage({
+        type: "getQuestionResult",
+        question: undefined,
+      });
+    }
+
+    const questionMapValues = Object.values(questionMap);
+    const foundQuestion = questionMapValues.find((question) => {
+      // TODO: compare answers
+      return (
+        question.task &&
+        getMaybeLocalizedValue(question.task) ===
+          getMaybeLocalizedValue(questionToFind.task)
+      );
+    });
+
+    window.postMessage({
+      type: "getQuestionResult",
+      question: foundQuestion,
+    });
+  };
+
+  const handlerMap = {
+    saveLesson: handleSaveLesson,
+    getQuestion: handleGetQuestion,
+  };
+  window.addEventListener("message", (messageEvent) => {
+    handlerMap[messageEvent.data.type]?.(messageEvent.data);
+  });
+};
+
+injectScript();
+sendIconUrl();
+listenForMessages();
--- src/injected.js
+++ src/injected.js
@@ -4,65 +4,69 @@ const courseId = window.location.href.match(
   /(?<=uu-coursekit-courseg01\/)[^\/]*/,
 )[0];
 
-const addRawTask = (task) => {
-  const getPlaceholder = (text) => `[raw_task:${encodeURIComponent(text)}]`;
-
-  if (!task.cs) return `${task}${getPlaceholder(task)}`;
-
-  return {
-    ...task,
-    cs: `${task.cs}${getPlaceholder(task.cs)}`,
-    en: `${task.en}${getPlaceholder(task.en)}`,
-  };
-};
-
-const handleLessonResponse = (response) => {
-  response
-    .clone()
-    .json()
-    .then((body) => {
-      window.postMessage(
-        { type: "loadLessonForStudenResponse", body, courseId },
-        "*",
-      );
-    });
-};
-
-const getLessonTextMiddleware = (response) => async () => {
-  const json = await response
-    .clone()
-    .json()
-    .then((data) => {
-      const mappedQuestionMapEntries = Object.entries(data.questionMap).map(
-        ([key, value]) => {
-          const mappedValue = {
-            ...value,
-            task: addRawTask(value.task),
-          };
-          return [key, mappedValue];
-        },
-      );
-      const mappedQuestionMap = Object.fromEntries(mappedQuestionMapEntries);
-      return { ...data, questionMap: mappedQuestionMap };
-    });
-  return JSON.stringify(json);
-};
-
-const getTestTextMiddleware = (response) => async () => {
-  const json = await response
-    .clone()
-    .json()
-    .then((data) => {
-      const mappedQuestionList = data.questionList.map((question) => ({
-        ...question,
-        task: addRawTask(question.task),
-      }));
-      return { ...data, questionList: mappedQuestionList };
-    });
-  return JSON.stringify(json);
-};
-
 const overrideFetch = () => {
+  const addRawQuestionPlacholderToTask = (question) => {
+    const { task } = question;
+    const placholder = `[raw_question:${encodeURIComponent(
+      JSON.stringify(question),
+    )}]`;
+
+    if (!task.cs) return `${task}${placholder}`;
+
+    return {
+      ...task,
+      cs: `${task.cs}${placholder}`,
+      en: `${task.en}${placholder}`,
+    };
+  };
+
+  const handleLessonResponse = (response) => {
+    response
+      .clone()
+      .json()
+      .then((body) => {
+        window.postMessage(
+          { type: "loadLessonForStudenResponse", lesson: body, courseId },
+          "*",
+        );
+      });
+    alert("uuHappiness: Loaded");
+  };
+
+  const getLessonTextMiddleware = (response) => async () => {
+    const json = await response
+      .clone()
+      .json()
+      .then((data) => {
+        const mappedQuestionMapEntries = Object.entries(data.questionMap).map(
+          ([key, question]) => {
+            const mappedQuestion = {
+              ...question,
+              task: addRawQuestionPlacholderToTask(question),
+            };
+            return [key, mappedQuestion];
+          },
+        );
+        const mappedQuestionMap = Object.fromEntries(mappedQuestionMapEntries);
+        return { ...data, questionMap: mappedQuestionMap };
+      });
+    return JSON.stringify(json);
+  };
+
+  const getTestTextMiddleware = (response) => async () => {
+    const json = await response
+      .clone()
+      .json()
+      .then((data) => {
+        const mappedQuestionList = data.questionList.map((question) => ({
+          ...question,
+          task: addRawQuestionPlacholderToTask(question),
+        }));
+        return { ...data, questionList: mappedQuestionList };
+      });
+    return JSON.stringify(json);
+  };
+
   window.fetch = async (...args) => {
     const response = await origFetch(...args);
     const url = response.url;
@@ -79,24 +83,230 @@ const overrideFetch = () => {
 };
 
 const watchCurrentQuestion = () => {
+  let currentRawQuestion;
+
   setInterval(() => {
-    const containerHtml = document.querySelector(
+    const container = document.querySelector(
       "div[class*=uu-coursekit-question-t]",
-    )?.innerHTML;
+    );
+    const containerHtml = container?.innerHTML;
     if (!containerHtml) return;
 
-    const rawTask = containerHtml.match(/(?<=\[raw_task:)[^\]]+(?=\])/)?.[0];
-    if (!rawTask) return;
+    const rawQuestion = containerHtml.match(
+      /(?<=\[raw_question:)[^\]]+(?=\])/,
+    )?.[0];
+    if (!rawQuestion) return;
+    if (currentRawQuestion === rawQuestion) return;
+    currentRawQuestion = rawQuestion;
 
-    const rawTaskDecoded = decodeURIComponent(rawTask);
+    const rawQuestionDecoded = JSON.parse(decodeURIComponent(rawQuestion));
 
     window.postMessage({
-      type: "questionChange",
-      task: rawTaskDecoded,
+      type: "getQuestion",
+      question: rawQuestionDecoded,
       courseId,
     });
+    document.getElementById(DIALOG_ID)?.remove();
+  }, 100);
+};
+
+const DIALOG_ID = "uuHappiness_dialog";
+const addAnswerUi = () => {
+  const CONTAINER_ID = "uuHappiness_container";
+
+  let iconUrl;
+  let question;
+  window.addEventListener("message", (messageEvent) => {
+    if (messageEvent.data.type === "iconUrl")
+      iconUrl = messageEvent.data.iconUrl;
+    if (messageEvent.data.type === "getQuestionResult")
+      question = messageEvent.data.question;
+  });
+
+  const getMaybeLocalizedValue = (task) => {
+    if (typeof task === "string") return task;
+
+    return task.cs;
+  };
+  const isDefined = (value) => value !== undefined && value !== null;
+
+  const makeDiv = (content) => {
+    const div = document.createElement("div");
+    if (Array.isArray(content)) div.append(...content);
+    else div.append(content);
+    return div;
+  };
+  const makeUl = (items) => {
+    const ul = document.createElement("ul");
+    const mappedItems = items.map((item) => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      ul.append(li);
+      return li;
+    });
+    ul.append(...mappedItems);
+    return ul;
+  };
+  const makeOl = (items) => {
+    const ol = document.createElement("ol");
+    const mappedItems = items.map((item) => {
+      const li = document.createElement("li");
+      li.textContent = item;
+      ol.append(li);
+      return li;
+    });
+    ol.append(...mappedItems);
+    return ol;
+  };
+  const makeHr = () => {
+    const hr = document.createElement("hr");
+    hr.style.cssText = "background-color: #BDBDBD";
+    return hr;
+  };
+
+  const renderUknownQuestion = () => {
+    return "Don't know answer for this question. Maybe you forgot to load it?";
+  };
+  const renderSingleAnswerFromList = () => {
+    return getMaybeLocalizedValue(
+      question.answerList[question.correctAnswerIndex],
+    );
+  };
+  const renderYesNoAnswer = () => {
+    return question.correctAnswerIndex === 0 ? "Ano" : "Ne";
+  };
+  const renderMultiPlaceholderAnswer = () => {};
+  const renderMultiAnswer = () => {
+    const correctAnswers = question.answerList
+      .filter((_, index) => question.correctAnswerIndexList.includes(index))
+      .map(getMaybeLocalizedValue);
+    if (correctAnswers.length > 0) return makeUl(correctAnswers);
+
+    return "Žádná z odpovědí není správná.";
+  };
+  const renderOrderAnswer = () => {
+    const answersInOrder = question.correctAnswerOrder.map((answerIndex) =>
+      getMaybeLocalizedValue(question.answerList[answerIndex]),
+    );
+
+    return makeOl(answersInOrder);
+  };
+  const renderPairAnswer = () => {
+    return question.pairList.map((pair, index) => {
+      const first = getMaybeLocalizedValue(
+        question.answerList[0][pair.answerIndex],
+      );
+      const second = getMaybeLocalizedValue(
+        question.answerList[1][pair.pairAnswerIndex],
+      );
+      const children = [makeUl([first, second])];
+
+      const isLast = question.pairList.length === index + 1;
+      if (!isLast) {
+        children.push(makeHr());
+      }
+
+      return makeDiv(children);
+    });
+  };
+  const renderTripletAnswer = () => {
+    question.tripletList.map((triplet) => {
+      const first = getMaybeLocalizedValue(question.answerList[0][triplet[0]]);
+      const second = getMaybeLocalizedValue(question.answerList[1][triplet[1]]);
+      const third = getMaybeLocalizedValue(question.answerList[2][triplet[2]]);
+      const children = [makeUl([first, second, third])];
+
+      const isLast = question.pairList.length === index + 1;
+      if (!isLast) {
+        children.push(makeHr());
+      }
+
+      return makeDiv(children);
+    });
+  };
+  const renderUnknownQuestionType = () => {
+    return "Don't know how to process this type of question :(";
+  };
+
+  const openDialog = () => {
+    const dialog = document.createElement("div");
+    dialog.id = DIALOG_ID;
+    dialog.style.cssText =
+      "width: 400px; background: white; border-radius: 8px; margin-bottom: 8px; box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 16px 0px;";
+    document.getElementById(CONTAINER_ID).prepend(dialog);
+
+    const title = document.createElement("div");
+    title.textContent = "Answer";
+    title.style.cssText =
+      "font-weight: bold; font-size: 18px; padding: 20px; background: #F5F5F5; color: #5B6071; text-align: center; border-top-left-radius: 8px; border-top-right-radius: 8px;";
+    dialog.append(title);
+
+    const answerContainer = document.createElement("div");
+    answerContainer.style.cssText = "padding: 20px; color: #5E646A;";
+    dialog.append(answerContainer);
+
+    if (!question) return answerContainer.append(renderUknownQuestion());
+    if (isDefined(question.correctAnswerIndex) && question.answerList)
+      return answerContainer.append(renderSingleAnswerFromList());
+    if (isDefined(question.correctAnswerIndex))
+      return answerContainer.append(renderYesNoAnswer());
+    if (
+      question.correctAnswerIndexList &&
+      Array.isArray(question.answerList[0])
+    )
+      return answerContainer.append(renderMultiPlaceholderAnswer());
+    if (question.correctAnswerIndexList)
+      return answerContainer.append(renderMultiAnswer());
+    if (question.correctAnswerOrder)
+      return answerContainer.append(renderOrderAnswer());
+    if (question.pairList) return answerContainer.append(...renderPairAnswer());
+    if (question.tripletList)
+      return answerContainer.append(renderTripletAnswer());
+
+    answerContainer.append(renderUnknownQuestionType());
+  };
+  const closeDialog = () => {
+    document.getElementById(DIALOG_ID).remove();
+  };
+  const toggleDialog = () => {
+    if (!!document.getElementById(DIALOG_ID)) {
+      closeDialog();
+      return;
+    }
+    openDialog();
+  };
+
+  setInterval(() => {
+    if (!iconUrl) return;
+    const isQuestion = !!document.querySelector(
+      "div[class*=uu-coursekit-question-t]",
+    );
+    if (!isQuestion) {
+      document.getElementById(CONTAINER_ID)?.remove();
+      return;
+    }
+
+    const containerExists = !!document.getElementById(CONTAINER_ID);
+    if (containerExists) return;
+
+    const container = document.createElement("div");
+    container.id = CONTAINER_ID;
+    container.style.cssText =
+      "position: fixed; bottom: 8px; right: 8px; display: flex; flex-direction: column; align-items: flex-end;";
+    document.body.append(container);
+
+    const button = document.createElement("button");
+    button.style.cssText = "background: none; border: none; cursor: pointer;";
+    button.onclick = toggleDialog;
+    container.append(button);
+
+    const icon = document.createElement("img");
+    icon.src = iconUrl;
+    icon.style.cssText = "border-radius: 100%; width: 50px;";
+    button.append(icon);
   }, 100);
 };
 
 overrideFetch();
 watchCurrentQuestion();
+addAnswerUi();
